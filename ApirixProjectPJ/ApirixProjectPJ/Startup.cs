01c8259ceee0a974bece4cbb0bdd0309d830d33b
Erik Petersson <erik__petersson@hotmail.com>
2016-05-17 09:20:14 +0200

    testupdate

---
+++ ApirixProjectPJ/ApirixProjectPJ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApirixProjectPJ.Startup))]
+namespace ApirixProjectPJ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
